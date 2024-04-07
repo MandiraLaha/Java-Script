a30f1c0ba01c33bf85ec7364c034a7eb2598ca26
MandiraLaha <mandiralaha2@gmail.com>
2024-04-07 23:04:41 +0530

    new

--- array.js
+++ array.js
@@ -1,1 +1,15 @@
-let arr = [1, 2, 3, 4];
+let arr = ['suze', 'roz', 'kit', 'rozy'];
+// let result = arr.map((item) => {
+//     return 'friend' + item;
+// })
+// console.log(result);
+// let arr2 = [1, 2, 3, 4];
+// let result = arr2.filter((item) => {
+//     return item % 2 === 0;
+// })
+// console.log(result);
+let result = arr.filter((item) => {
+    return item.length === 3;
+})
+console.log(result);
+
