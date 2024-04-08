bb1ea01873f2371a21a8dc4a0dfd4bfe2e2b8915
MandiraLaha <mandiralaha2@gmail.com>
2024-04-08 14:46:27 +0530

    new

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+let a = '121';
+console.log(a.split(''));
+let b = [1, 2, 3];
+console.log(b.reverse().join(''));
+let c = 121;
+console.log(c.toString().split(''));
+let d = 1234;
+console.log(d.toString().split('').reverse().join(''));
